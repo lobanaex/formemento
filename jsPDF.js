f2653bb62c3d5bdcd9bfd1786e326c8f38bb441e
lobanaex <lobanov@gmx.de>
2017-07-11 08:23:16 +0200

    Update jsPDF.js

--- jsPDF.js
+++ jsPDF.js
@@ -1,61 +1,3 @@
-
-
-* @preserve
-* jsPDF - PDF Document creation from JavaScript
-* Version ${versionID}
-* CommitID ${commitID}
-
-* Copyright (c) 2010-2016 James Hall <james@parall.ax>, https://github.com/MrRio/jsPDF
-* 2010 Aaron Spike, https://github.com/acspike
-* 2012 Willow Systems Corporation, willow-systems.com
-* 2012 Pablo Hess, https://github.com/pablohess
-*               2012 Florian Jenett, https://github.com/fjenett
-*               2013 Warren Weckesser, https://github.com/warrenweckesser
-*               2013 Youssef Beddad, https://github.com/lifof
-*               2013 Lee Driscoll, https://github.com/lsdriscoll
-*               2013 Stefan Slonevskiy, https://github.com/stefslon
-*               2013 Jeremy Morel, https://github.com/jmorel
-*               2013 Christoph Hartmann, https://github.com/chris-rock
-*               2014 Juan Pablo Gaviria, https://github.com/juanpgaviria
-*               2014 James Makes, https://github.com/dollaruw
-*               2014 Diego Casorran, https://github.com/diegocr
-*               2014 Steven Spungin, https://github.com/Flamenco
-*               2014 Kenneth Glassey, https://github.com/Gavvers
-*
-* Permission is hereby granted, free of charge, to any person obtaining
-* a copy of this software and associated documentation files (the
-* "Software"), to deal in the Software without restriction, including
-* without limitation the rights to use, copy, modify, merge, publish,
-* distribute, sublicense, and/or sell copies of the Software, and to
-* permit persons to whom the Software is furnished to do so, subject to
-* the following conditions:
-*
-* The above copyright notice and this permission notice shall be
-* included in all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-* NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-* LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-* OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-* WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*
-* Contributor(s):
-*    siefkenj, ahwolf, rickygu, Midnith, saintclair, eaparango,
-*    kim3er, mfo, alnorth, Flamenco
-*/
-**
-* Creates new jsPDF document object instance.
-*
-* @class
-* @param orientation One of "portrait" or "landscape" (or shortcuts "p" (Default), "l")
-* @param unit        Measurement unit to be used when coordinates are specified.
-*                    One of "pt" (points), "mm" (Default), "cm", "in"
-* @param format      One of 'pageFormats' as shown below, default: a4
-* @returns {jsPDF}
-* @name jsPDF
-*/
 var jsPDF = (function(global) {
 'use strict';
 var pdfVersion = '1.3',
@@ -1448,7 +1390,7 @@ out('W');
 }
 // End the path object without filling or stroking it.
 // This operator is a path-painting no-op, used primarily for the side effect of changing the current clipping path
-// (see Section 4.4.3, “Clipping Path Operators”)
+// (see Section 4.4.3, Â“Clipping Path OperatorsÂ”)
 out('n');
 };
 /**
